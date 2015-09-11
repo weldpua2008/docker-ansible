304168daf8097a585a966f8e6029d1b309048c5d
Valery Solovyov <weldpua2008@gmail.com>
2015-09-11 07:33:38 +0400

    created the script for tests

---
+++ tests/runtests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+############################
+#
+# Valeriy Solovyov <weldpua2008@gmail.com>
+# version 0.1 <11.09.2015>
+#   - init
+############################
+OS_TYPE=${1:-}
+OS_VERSION=${2:-}
+if [ "${OS_TYPE}" == "centos" ];then
+    OS_PATH_PREFIX="centos/${OS_VERSION}/"
+elif [ "${OS_TYPE}" == "ubuntu_lts" ];then
+    OS_PATH_PREFIX="ubuntu/LTS/${OS_VERSION}/"
+elif [ "${OS_TYPE}" == "ubuntu" ];then
+    OS_PATH_PREFIX="ubuntu/${OS_VERSION}/"
+fi
+TEMP_DOCKER_IMAGE_NAME="test_weldpua2008/${OS_TYPE}_${OS_VERSION}"
+
+
+echo ${PWD}
+cd ..
+docker build -t ${TEMP_DOCKER_IMAGE_NAME} ${OS_PATH_PREFIX}&& docker run --rm=true ${TEMP_DOCKER_IMAGE_NAME} /bin/bash -c "ansible --version" || echo "failed" && exit 127
