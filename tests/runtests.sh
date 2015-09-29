8b235d22920ad611668c6f592180e0cb2a468039
Valeriy Solovyov <weldpua2008@gmail.com>
2015-09-29 13:57:39 +0300

    added distros: alpine; debian

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -7,26 +7,26 @@
 ############################
 OS_TYPE=${1:-}
 OS_VERSION=${2:-}
-if [ "${OS_TYPE}" == "centos" ];then
-    OS_PATH_PREFIX="centos/${OS_VERSION}/"
-elif [ "${OS_TYPE}" == "ubuntu_lts" ];then
+if [ "${OS_TYPE}" == "ubuntu_lts" ];then
     OS_PATH_PREFIX="ubuntu/LTS/${OS_VERSION}/"
-elif [ "${OS_TYPE}" == "ubuntu" ];then
-    OS_PATH_PREFIX="ubuntu/${OS_VERSION}/"
-elif [ "${OS_TYPE}" == "fedora" ];then
-    OS_PATH_PREFIX="fedora/${OS_VERSION}/"
-elif [ "${OS_TYPE}" == "opensuse" ];then
-    OS_PATH_PREFIX="opensuse/${OS_VERSION}/"
+#elif [ "${OS_TYPE}" == "centos" ];then
+#    OS_PATH_PREFIX="centos/${OS_VERSION}/"
+#elif [ "${OS_TYPE}" == "ubuntu" ];then
+#    OS_PATH_PREFIX="ubuntu/${OS_VERSION}/"
+#elif [ "${OS_TYPE}" == "fedora" ];then
+#    OS_PATH_PREFIX="fedora/${OS_VERSION}/"
+#elif [ "${OS_TYPE}" == "opensuse" ];then
+#    OS_PATH_PREFIX="opensuse/${OS_VERSION}/"
 else
-    #if [ ! -d  "${OS_TYPE}/${OS_VERSION}/ ];then
-    echo "unsupported OS_TYPE: ${OS_TYPE}"
-    exit 126
+    OS_PATH_PREFIX="${OS_TYPE}/${OS_VERSION}/"
+    if [ ! -d  "${OS_PATH_PREFIX}" ];then
+        echo "unsupported OS_TYPE: ${OS_TYPE}"
+        exit 126
+    fi
 fi
 TEMP_DOCKER_IMAGE_NAME="test_weldpua2008/${OS_TYPE}_${OS_VERSION}"
-
-
-echo ${PWD}
-ls -la ${OS_PATH_PREFIX}
+#echo ${PWD}
+#ls -la ${OS_PATH_PREFIX}
 echo "docker build -t ${TEMP_DOCKER_IMAGE_NAME} ${OS_PATH_PREFIX} && docker run ${TEMP_DOCKER_IMAGE_NAME} ansible --version"
 #cd ..
 docker build -t ${TEMP_DOCKER_IMAGE_NAME} ${OS_PATH_PREFIX}&& docker run ${TEMP_DOCKER_IMAGE_NAME} /bin/bash -c "ansible --version" && echo "success" && exit 0 || echo "failed" && exit 127
